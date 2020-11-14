e149d6f0c291e733b6a9701d8f97c25953397001
mello1994 <neoenda@gmail.com>
2020-11-14 15:20:42 +0900

    implement events index

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,10 @@ import axios from 'axios'
 
 export const READ_EVENTS = 'READ_EVENTS'
 
-// const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
-// const QUERYSTRING = '?token=token123'
+const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
+const QUERYSTRING = '?token=token123'
 
 export const readEvents = () => async dispatch => {
-        const response = await axios.get('https://udemy-utils.herokuapp.com/api/v1/events?token=token123')
-        console.log(response)
+        const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
         dispatch({ type: READ_EVENTS, response})
     }
