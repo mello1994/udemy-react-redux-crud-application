412f637cf08f36295341141cd6c80c03e7b8050f
mello1994 <neoenda@gmail.com>
2020-11-14 13:54:55 +0900

    fix

---
+++ src/components/events_index.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux'
+import { readEvents } from '../actions'
+
+class EventsIndex extends Component {
+  componentDidMount() {
+    this.props.readEvents()
+  }
+  render() {
+    const props = this.props
+    return (
+      <React.Fragment>
+        <div>{ console.log(props.events) }</div>
+      </React.Fragment>
+    )
+  }
+}
+
+const mapStateToProps = state => ({ events: state.events })
+
+const mapDispatchToProps = dispatch => ({ readEvents})
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
